227cc57061bf250e3c659864fcaf7bf752060a12
alex10alex <t0983781367@gmail.com>
2017-10-22 13:45:45 +0300

    Vk_Bot

--- Bot_test_File.py
+++ Bot_test_File.py
@@ -1,1 +1,12 @@
-a=1
+import vk_api
+import time
+
+
+vk = vk_api.VkApi(token = "3676fce2670eb9d72e058bce5821182a6762821f3e06b6acf178d9d4b380b54dce30b72ab180a2c421157")
+vk.auth()
+
+values = {'out':0, 'count':100, 'time_offset':60}
+vk.method('messages.get', values)
+
+def write_msg(user_id, s):
+    vk.method('messages.send', {'user_id':user_id, 'messages':s})
