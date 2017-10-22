2f80c003a13373152b399e932b5556c4b0a14ad2
alex10alex <t0983781367@gmail.com>
2017-10-22 14:49:42 +0300

    Vk_Bot

--- Bot_test_File.py
+++ Bot_test_File.py
@@ -3,10 +3,18 @@ import time
 
 
 vk = vk_api.VkApi(token = "3676fce2670eb9d72e058bce5821182a6762821f3e06b6acf178d9d4b380b54dce30b72ab180a2c421157")
-vk.auth()
+vk._auth_token()
 
 values = {'out':0, 'count':100, 'time_offset':60}
-vk.method('messages.get', values)
 
 def write_msg(user_id, s):
     vk.method('messages.send', {'user_id':user_id, 'messages':s})
+
+while True:
+    response = vk.method('messages.get', values)
+    if response['items']:
+        values['last_message_id'] = response['items'][0]['id']
+    for item in response['items']:
+        write_msg(item['user_id'], 'Привет, человек')
+    time.sleep(1)
+
